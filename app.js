9f909104043f0205e9bb03f74a64afafd09390d7
MelindaKleszken <melindakleszken1021@gmail.com>
2020-12-16 12:19:13 +0000

    responsive design

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const clap = document.getElementById("a");
 const hihat = document.getElementById("s");
 const kick = document.getElementById("d");
@@ -28,7 +29,27 @@ document.addEventListener("keydown", (i) => {
         tom.play();
     } else if (i.key === "l") {
         tink.play();
-    } else {
+    } 
+});
+
+document.addEventListener("click", (i) => { 
+    if (i.key === "a") {
         clap.play();
-    }
+    } else if (i.key === "s") {
+        hihat.play();
+    } else if (i.key === "d") {
+        kick.play();
+    } else if (i.key === "f") {
+        openhat.play();
+    } else if (i.key === "g") {
+        boom.play();
+    } else if (i.key === "h") {
+        ride.play();
+    } else if (i.key === "j") {
+        snare.play();
+    } else if (i.key === "k") {
+        tom.play();
+    } else if (i.key === "l") {
+        tink.play();
+    } 
 });
