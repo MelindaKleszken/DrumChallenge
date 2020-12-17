26605745768530c82b1e2889d411fc78b6144e4c
MelindaKleszken <melindakleszken1021@gmail.com>
2020-12-17 15:24:40 +0000

    click works - only 1 sound

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const ride = document.getElementById("h");
 const snare = document.getElementById("j");
 const tom = document.getElementById("k");
 const tink = document.getElementById("l");
-const button = document.querySelectorAll(".button");
-console.log(button);
+
 
 
 
@@ -36,32 +35,10 @@ document.addEventListener("keydown", (i) => {
 });
 
 
-//play sounds by clicking keys ----this part still need a check
-for (let i = 0; i < button.length; i++) {
-    document.addEventListener("click", function () {
-    
-    let letter = this[i].id;
-    
-    //console.log(i);
-    console.log(letter);
-    
-    });
+var button = document.getElementsByClassName('button'); 
+for (var i = 0; i < button.length; i++) { 
+  button[i].addEventListener('click', function(e) {
+    console.log(i);
+    clap.play();
+  }); 
 }
-/*function playSound() {
-    var sound = button.getElementById("audio");
-    sound.play();
-}*/
-
-
-
-/*
-function myFunction() {
-    let button = document.querySelectorAll(".button")[0].id;
-    console.log(button);
-    let letter = "";
-    
-    for (let i = 0; i < button.attributes.length; i++) {
-      letter = letter + button.attributes[i].name + " = " + button.attributes[i].value + "<br>";
-    }
-    this.innerHTML = letter;
-  }*/
